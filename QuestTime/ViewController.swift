b882ebf3fba2276e9da1ea110f54445b298c7a55
Dominik Cubelic <dominik.cubelic@q-alliance.com>
2018-03-04 14:18:30 +0100

    Initial Commit

---
+++ QuestTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuestTime
+//
+//  Created by dominik on 04/03/2018.
+//  Copyright © 2018 BlabLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
