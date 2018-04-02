7929949980f684968221ff292681ac924ff87e39
Dominik Cubelic <dominik.cubelic@q-alliance.com>
2018-04-02 14:39:42 +0200

    More UI Changes

--- QuestTime/RegisterViewController.swift
+++ QuestTime/RegisterViewController.swift
@@ -7,6 +7,17 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextField: QTTextField!
     @IBOutlet weak var repeatPasswordTextField: QTTextField!
     
+    @IBOutlet weak var textFieldsViewCenterYConstraint: NSLayoutConstraint!
+    @IBOutlet weak var logoWidthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var separatorViewCenterYConstraint: NSLayoutConstraint!
+    
+    var keyboardObserver: NSObjectProtocol?
+    deinit {
+        if let keyboardObserver = keyboardObserver {
+            NotificationCenter.default.removeObserver(keyboardObserver)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +29,28 @@ class RegisterViewController: UIViewController {
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapAction))
         view.addGestureRecognizer(tapGesture)
+        
+        keyboardObserver = NotificationCenter.default.addObserver(forName: .UIKeyboardWillChangeFrame, object: nil, queue: nil, using: { (notification) in
+            if let userInfo = notification.userInfo,
+                let durationValue = userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber,
+                let endFrameValue = userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue,
+                let curve = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber {
+                
+                if endFrameValue.cgRectValue.minY == self.view.frame.height {
+                    self.logoWidthConstraint.constant = 150
+                    self.separatorViewCenterYConstraint.constant = 0
+                    self.textFieldsViewCenterYConstraint.constant = 0
+                } else {
+                    self.logoWidthConstraint.constant = 0
+                    self.separatorViewCenterYConstraint.constant = -130
+                    self.textFieldsViewCenterYConstraint.constant = -65
+                }
+                
+                UIView.animate(withDuration: durationValue.doubleValue, delay: 0, options: UIViewAnimationOptions(rawValue: UInt(curve.intValue << 16)), animations: {
+                    self.view.layoutIfNeeded()
+                }, completion: nil)
+            }
+        })
     }
     
     @objc func tapAction() {
